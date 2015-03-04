80e44e2bad12bef7d3e98eceedb3028af71372e0
chuck <charlesrymal@gmail.com>
2015-03-04 11:25:19 -0800

    move net

--- config.py
+++ config.py
@@ -1,11 +1,14 @@
-l = Static("nebula_net")
+
+l = Dynamic("nebula_plugin_net1")
 
 l.inc_dirs.append("/usr/include/freetype2")
 
-l.require("galaxy_std")
-l.require("galaxy_log")
-l.require("galaxy_net")
-l.require("nebula_core")
+l.require("galaxy_std", whole=True)
+l.require("galaxy_log", whole=True)
+l.require("galaxy_console", whole=True)
+l.require("nebula_core", whole=True)
 
 l.make()
+
+
 
